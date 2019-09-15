9fc32ab0de2124c4f50d592f7db96e4906965660
Antoine <antwane_demon@yahoo.fr>
2019-09-15 14:35:13 +0200

    update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@ class PagesController < ApplicationController
   def home
 	@variable = 4
   end
-  def index
+  def deplacements
 	@travels = Travel.all
   end
   def create
     Travel.create from: params[:from], to: params[:to], distance: params[:distance]
-    redirect_to "pages/deplacements"
+    redirect_to "/deplacements"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'travels/index'
   get 'pages/home'
-  get 'deplacements' => 'pages#index'
+  get 'deplacements' => 'pages#deplacements'
+  #get 'pages/deplacements'
   post 'deplacements' => 'pages#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
